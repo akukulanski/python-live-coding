b2d0500f99034e9a811724cca697b374ae2d241c
akukulanski <akukulanski@gmail.com>
2019-09-08 23:55:50 -0300

    playing

--- ejemplos.eq
+++ ejemplos.eq
@@ -1,13 +1,32 @@
+Single tone (1KHz):
 0.2*sin(2*pi*1000*t)
 
+1 KHz tone + 700 Hz tone:
 0.05*sin(2*pi*1000*t)+0.025*sin(2*pi*700*t)
+
+1 KHz square wave:
+0.01 * mod(2*t*1e3,2)
+
+
+1 KHz that for 0.25 seconds and off for 0.5 seconds:
+0.01 * mod(2*t*1e3,2) * (mod(t/0.25,3)==0)
+
+
+0.01 * (floor(2*t*1e3)%2) * ((floor(t/0.25)%3)==0) + 0.02 * (floor(2*t*2e3)%2) * ((floor(t/0.125+3)%4)==0)
+
+0.05*sin(2*pi*500*(1+0.5*mod(t/0.5,4))*t)
 
 0.05*sin(2*pi*1000*t)+0.025*sin(2*pi*700*t)+0.03*(t*1000%2) * sin(2*pi*(t*1000%4))
 
-0
-
 0.05*sin(2*pi*1000*t)+0.025*sin(2*pi*700*t)+0.01*(t*1000%2) * sin(2*pi*(t*1000%4))
 
+falopa:
 0.1*sin(2*pi*(t*10000%4)*t)
 
+0.1*sin(2*pi*(mod(t*10000,4))*t)
+
+mute:
+0
+
+exit:
 quit
--- music-eq.py
+++ music-eq.py
@@ -1,16 +1,21 @@
 import sys
 import numpy as np
 import sounddevice as sd
-from numpy import sin, cos, pi
+from numpy import sin, cos, pi, floor
 import time
 import clipboard
 
 samplerate = 44100 #sd.query_devices(args.device, 'output')['default_samplerate']
 start_idx = 0
 
+mod = lambda a,b: np.floor(a%b)
+
 def callback(outdata, frames, time, status):
     if status:
-        print(status, file=sys.stderr)
+        print('status=', status, file=sys.stderr)
+        print('frames=', frames, file=sys.stderr)
+        print('time=', time, file=sys.stderr)
+        print('len outdata=', len(outdata), file=sys.stderr)
     global start_idx
     global formula
     t = (start_idx + np.arange(frames)) / samplerate
@@ -36,10 +41,11 @@ def reproducir(callback, samplerate):
                 formula_nueva(0.0) # check that syntax is ok
                 return formula_nueva
             except:
+                print('Could not evaluate: {}'.format(clipboard_actual))
                 continue
 
 print('A jugar!')
-formula = lambda t: eval('0.2 * sin(2 * pi * 500 * t)')
+formula = lambda t: eval('0.05 * sin(2 * pi * 500 * t)')
 formula_anterior = formula
 while True:
     try:
